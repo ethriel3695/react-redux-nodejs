a22853909815cd248f7d045dc3d10eaf6c30b9fd
GOVERNET\reubenellis <rellis@governet.net>
2017-10-10 16:37:22 -0600

    adding notes to index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 // import React from 'react';
+
+// https://github.com/coryhouse/pluralsight-redux-starter/tree/master/src
+// https://github.com/stukent/chump
+
 import routes from './routes.js';
 
 ReactDOM.render(
